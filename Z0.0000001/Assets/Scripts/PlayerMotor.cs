817437acd514f128268136355493f58de3de6499
Kyle Bue <kylebue9@gmail.com>
2018-01-23 15:32:35 -0800

    Added test scene; Made controller script; Added start animation

---
+++ Z0.0000001/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float speed = 7.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+
+    private float animationDuration = 3.0f;
+
+    public float Speed { get { return speed; } set { Speed = speed; } }
+
+	// Use this for initialization
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0.0f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
