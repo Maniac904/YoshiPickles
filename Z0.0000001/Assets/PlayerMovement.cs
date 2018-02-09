1c3722a7d7159c3d1f363ac01e05a82f26da93ab
Lewis Griffith <griffymp@yahoo.com>
2018-02-09 10:08:21 -0800

    added some script
    doesnt work yet yay

--- Z0.0000001/Assets/PlayerMovement.cs
+++ Z0.0000001/Assets/PlayerMovement.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed = 0f;
+	public Rigidbody cHrB;
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		cHrB = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		transform.position = Vector3(transform.forward * speed);
 	}
 }
